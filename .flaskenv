55785d443ab1fafdb07305d62a047cd2cb65fa29
JosePedro <josepedro88@live.com.pt>
2020-02-23 19:10:03 +0000

    index and route created

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=toDo.py
+FLASK_ENV=development
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'To do list'
+    notes = ['Note 1', 'Note 2', 'Note 4']
+    return render_template('index.html', notes=notes)
