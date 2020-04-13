7e60489c01a56406438200ca5933d28877e4cecc
JosePedro <josepedro88@live.com.pt>
2020-04-13 21:02:51 +0100

    gitignore added

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\josep\OneDrive\Documents\Programming\Portfolio\toDo\env
+set VIRTUAL_ENV=C:\Users\josep\OneDrive\Documents\Programming\Portfolio\FlasktodoList\env
 
 if not defined PROMPT set PROMPT=$P$G
 
